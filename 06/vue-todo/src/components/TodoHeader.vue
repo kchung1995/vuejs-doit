90579a900fa483c4ae501c1eb2d8cbded790538c
KuyHo <kchung1995@gmail.com>
2022-08-30 22:25:53 +0900

    6.실전 애플리케이션 만들기(2)

--- 06/vue-todo/src/components/TodoHeader.vue
+++ 06/vue-todo/src/components/TodoHeader.vue
@@ -1,9 +1,34 @@
 <template>
-  <div>header</div>
+  <header>
+    <h1>TODO it!</h1>
+  </header>
 </template>
 
 <script>
 export default {};
 </script>
 
-<style></style>
+<style>
+body {
+  text-align: center;
+  background-color: #f6f6f8;
+}
+input {
+  border-style: groove;
+  width: 200px;
+}
+button {
+  border-style: groove;
+}
+.shadow {
+  box-shadow: 5px 10px 10px rgba(0, 0, 0, 0.3);
+}
+</style>
+
+<style scoped>
+h1 {
+  color: #2f3b52;
+  font-weight: 900;
+  margin: 2.5rem 0 1.5rem;
+}
+</style>
